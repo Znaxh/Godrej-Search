b9291f49f12f437ef5d5e06814e8a0312af6c9ee
Znaxh <anuragpratapsingh.4845@gmail.com>
2024-09-20 23:07:08 +0530

    third commit

--- components/Footer.js
+++ components/Footer.js
@@ -8,9 +8,9 @@ function Footer() {
       <div className="grid grid-col-1 md:grid-cols-2 lg:grid-cols-3 gap-y-4 grid-flow-row-dense px-8 py-3">
         <div className="flex link justify-center items-center md:col-span-2 lg:col-span-1 lg:col-start-2 font-Ubuntu">
           <img src="github-brands.png" className="h-5  px-1 "></img>{" "}
-          <a href="https://myaccount.google.com/security-checkup">
+          <a href="https://github.com/Znaxh/Godrej-Search">
             {" "}
-            Github.com/cosmicwanderer7{" "}
+            Github.com/Godrej-Search{" "}
           </a>
         </div>
 
